03d104f9739161b5f81ada6cb2901477dbad574e
Alice <alicesb2006@gmail.com>
2025-01-27 17:05:44 -0300

    Creating the structure style using Grid

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,13 @@ import BtnCalc from "./components/BtnCalc"
 function App() {
   return (
     <>
-      <div className="container-calc">
-        <div className="output">
-          <div className="prev-operation"></div>
-          <div className="current-operation"></div>
+      <div className="grid gridPersonalizeRow gridPersonalizeCol border-y-[50px] border-x-[30px] rounded-md bg-black gap-5 shadow-2xl">
+        <div className="col-span-4 bg-[#a2bf51] flex flex-col items-end justify-center p-2 break-all rounded-md">
+          <div className="text-[#ffffff88] text-md">1111 x</div>
+          <div className="text-white text-4xl">111</div>
         </div>
         <BtnCalc isBig numCalc="AC" />
-        <BtnCalc numCalc="DEL" />
+        <BtnCalc numCalc="C" />
         <BtnCalc numCalc="%" />
         <BtnCalc numCalc="1" />
         <BtnCalc numCalc="2" />
