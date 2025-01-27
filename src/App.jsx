f96cd909e0c2be7b076e8047864f66eba9d1231e
Alice <alicesb2006@gmail.com>
2025-01-27 19:20:22 -0300

    Putting functions into the calculator's operation

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,37 @@
-import BtnCalc from "./components/BtnCalc"
+import { useReducer } from "react"
+import { ACTIONS, reducer } from "./utils/calUtils";
+import BtnOp from "./components/BtnOp";
+import BtnNum from "./components/BtnNum";
 
 function App() {
+
+  const[{curOperation, prevOperation, operation}, dispatch] = useReducer(reducer, {});
+
   return (
     <>
       <div className="grid gridPersonalizeRow gridPersonalizeCol border-y-[50px] border-x-[30px] rounded-xl bg-black gap-5 shadow-2xl">
         <div className="col-span-4 bg-[#a2bf51] flex flex-col items-end justify-center p-2 break-all rounded-md">
-          <div className="text-[#ffffffa3] text-md">1111 x</div>
-          <div className="text-white text-4xl">111</div>
+          <div className="text-[#ffffffa3] text-md">{prevOperation} {operation}</div>
+          <div className="text-white text-4xl">{curOperation || " " }</div>
         </div>
-        <BtnCalc isBig numCalc="AC" />
-        <BtnCalc isClear numCalc="C" />
-        <BtnCalc numCalc="%" />
-        <BtnCalc isNum numCalc="1" />
-        <BtnCalc isNum numCalc="2" />
-        <BtnCalc isNum numCalc="3" />
-        <BtnCalc numCalc="x" />
-        <BtnCalc isNum numCalc="4" />
-        <BtnCalc isNum numCalc="5" />
-        <BtnCalc isNum numCalc="6" />
-        <BtnCalc numCalc="+" />
-        <BtnCalc isNum numCalc="7" />
-        <BtnCalc isNum numCalc="8" />
-        <BtnCalc isNum numCalc="9" />
-        <BtnCalc numCalc="-" />
-        <BtnCalc numCalc="," />
-        <BtnCalc isNum numCalc="0" />
-        <BtnCalc isBig numCalc="=" />
+        <button onClick={() => dispatch({type: ACTIONS.CLEAR})} className="btn-big text-3xl bg-[#b3b3b3] text-white inset-shadow-sm inset-shadow-gray-700 rounded-2xl shadow-md shadow-gray-900 hover:inset-shadow-gray-900 hover:shadow-black duration-250 hover:scale-98 ease-in">AC</button>
+        <BtnOp isClear opCalc="C" dispatch={dispatch}/>
+        <BtnOp opCalc="%" dispatch={dispatch}/>
+        <BtnNum isNum numCalc="1" dispatch={dispatch}/>
+        <BtnNum isNum numCalc="2" dispatch={dispatch}/>
+        <BtnNum isNum numCalc="3" dispatch={dispatch}/>
+        <BtnOp opCalc="x" dispatch={dispatch}/>
+        <BtnNum isNum numCalc="4" dispatch={dispatch}/>
+        <BtnNum isNum numCalc="5" dispatch={dispatch}/>
+        <BtnNum isNum numCalc="6" dispatch={dispatch}/>
+        <BtnOp opCalc="+" dispatch={dispatch}/>
+        <BtnNum isNum numCalc="7" dispatch={dispatch}/>
+        <BtnNum isNum numCalc="8" dispatch={dispatch}/>
+        <BtnNum isNum numCalc="9" dispatch={dispatch}/>
+        <BtnOp opCalc="-" dispatch={dispatch}/>
+        <BtnNum numCalc="." dispatch={dispatch}/>
+        <BtnNum isNum numCalc="0" dispatch={dispatch}/>
+        <BtnOp isBig opCalc="=" dispatch={dispatch}/>
       </div>
     </>
   )
