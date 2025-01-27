b4f41dbbd2bc471a3106ed5216ab301342c35bdf
Alice <alicesb2006@gmail.com>
2025-01-27 17:45:58 -0300

    Modifying the button style

--- src/components/BtnCalc.jsx
+++ src/components/BtnCalc.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const BtnCalc = ({numCalc, isBig, isNum, isClear}) => {
   return (
-    <button className={`inset-shadow-sm ${isBig ? "btn-big" : "" } ${isNum ? "bg-black inset-shadow-gray-600" : isClear ? "bg-[#cc2d43]" : numCalc == "=" ? "bg-[#f8a13a]" : "bg-[#b3b3b3]"} text-3xl text-white inset-shadow-sm inset-shadow-gray-800 rounded-2xl`} >{numCalc}</button>
+    <button className={`inset-shadow-sm ${isBig ? "btn-big" : "" } ${isNum ? "bg-black inset-shadow-gray-600" : isClear ? "bg-[#cc2d43]" : numCalc == "=" ? "bg-[#f8a13a]" : "bg-[#b3b3b3]"} text-3xl text-white inset-shadow-sm inset-shadow-gray-700 rounded-2xl shadow-md shadow-gray-900 hover:inset-shadow-gray-900 hover:shadow-black duration-250 hover:scale-98 ease-in`} >{numCalc}</button>
   )
 }
 
